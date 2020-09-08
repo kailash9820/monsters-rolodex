a1ca4617ef074f76fe26e0f4c01425db899cc71d
kailash9820 <kailashgaribidi9820@gmail.com>
2020-09-08 20:50:38 +0530

    Updates

---
+++ precache-manifest.8f5bfa1f9d51a590993d9129ee0394bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3a4d0d814cc37efef5dbe84c346089",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "080b28a1847204245956",
+    "url": "/monsters-rolodex/static/css/main.c5601fb1.chunk.css"
+  },
+  {
+    "revision": "91ef67d745446be22fd7",
+    "url": "/monsters-rolodex/static/js/2.865bbbd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.865bbbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080b28a1847204245956",
+    "url": "/monsters-rolodex/static/js/main.f1482a34.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
